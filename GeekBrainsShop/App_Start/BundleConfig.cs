0498b6d5d7e5f41c87b8b83360a477d361db7bca
Дмитрий Горбовский <dimon007@inbox.ru>
2017-09-25 12:29:16 +0300

    Правка стилей

--- GeekBrainsShop/App_Start/BundleConfig.cs
+++ GeekBrainsShop/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace GeekBrainsShop
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
